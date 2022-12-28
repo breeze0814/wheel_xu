fd853cfa8fbb7dc1ed3eb63c8df3bdbaf08f52e8
账号名 <712127095@qq.com>
2022-12-28 16:35:12 +0800

    初始化路由router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <p>导航栏</p>
+  <router-link to='/'>Frank</router-link> &nbsp;
+  <router-link to='/xxx'>Frank2</router-link>
+  <hr />
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
