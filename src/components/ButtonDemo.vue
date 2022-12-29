0e89d138d54a28e80e0bd81fdc2ee6965dde5d07
账号名 <712127095@qq.com>
2022-12-29 19:04:59 +0800

    属性绑定

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,21 @@
 <template>
   <div>Button 示例</div>
-  <h1>实例</h1>
-  <Button>你好</Button>
+  <h1>示例1</h1>
+  <div>
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick">
+      你好
+    </Button>
+  </div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
 export default {
   components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
 };
 </script>
