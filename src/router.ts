4da8d10d0dd219e2d9f6bdaea207f45a45fcbb74
账号名 <712127095@qq.com>
2022-12-31 15:53:27 +0800

    创建子页面路由

--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,9 @@ import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
-
+import Intro from './views/Intro.vue';
+import GetStarted from './views/GetStarted.vue';
+import Install from './views/Install.vue';
 const history = createWebHashHistory();
 export const router = createRouter({
   history: history,
@@ -17,6 +19,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocDemo },
+        { path: "intro", component: Intro },
+        { path: "install", component: Install },
+        { path: "get-Started", component: GetStarted },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
