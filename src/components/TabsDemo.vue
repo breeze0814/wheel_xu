46fbd1940b38e5257c1afaf858f45fa519fb2ed7
账号名 <712127095@qq.com>
2022-12-31 12:36:44 +0800

    完成tabs组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,11 +1,21 @@
 <template>
   <div>Tabs 示例</div>
   <H1>示例1</H1>
+  <Tabs v-model:selected="x">
+    <Tab title="导航111">内容1</Tab>
+    <Tab title="导航2222222222222">内容2</Tab>
+  </Tabs>
 </template>
 <script lang="ts">
 import Tabs from "../lib/Tabs.vue";
 import Tab from "../lib/Tab.vue";
+import { ref } from "vue";
 export default {
-  component: { Tabs, Tab },
+  components: { Tabs, Tab },
+
+  setup(props) {
+    const x = ref("导航111");
+    return { x };
+  },
 };
 </script>
