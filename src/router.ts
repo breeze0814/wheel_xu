aa115256ed44f3d0e34f4ce4f25f1eb36e3fb757
账号名 <712127095@qq.com>
2022-12-31 17:41:40 +0800

    支持markdown

--- src/router.ts
+++ src/router.ts
@@ -9,7 +9,10 @@ import DocDemo from "./components/DocDemo.vue";
 import Intro from './views/Intro.vue';
 import GetStarted from './views/GetStarted.vue';
 import Install from './views/Install.vue';
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 const history = createWebHashHistory();
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
   history: history,
   routes: [
@@ -19,9 +22,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocDemo },
-        { path: "intro", component: Intro },
-        { path: "install", component: Install },
-        { path: "get-Started", component: GetStarted },
+        { path: "intro", component: md('intro') },
+        { path: "install", component: md('install') },
+        { path: "get-Started", component: md('get-started') },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
--- src/views/GetStarted.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    GetStarted
-  </div>
-</template>
